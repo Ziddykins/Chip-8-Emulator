1c80e029ca25ef31e0f35e6f7d80dea3c140ad4b
Ziddykins <Jeff0012@hotmail.com>
2015-05-06 05:44:13 -0400

    Update chip8.c

--- chip8.c
+++ chip8.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <unistd.h>
+
+#include <SDL2/SDL.h>
 #include "chip8.h"
 
 chip8 Chip8;
 
 unsigned char fontset[80] = { 
-  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-  0x20, 0x60, 0x20, 0x20, 0x70, // 1
-  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
 void initialize (void) {
@@ -51,10 +54,19 @@ void initialize (void) {
 
 void read_mem (void) {
     int i;
-    for (i=0; i<4096; i++) {
-        if (i % 15 == 0) printf("\n");
-        printf("%#04x ", Chip8.mem[i]);
+    printf("Mem:\n");
+    for (i=512; i<1296; i++) {
+        if (i % 25 == 0) printf("\n");
+        printf("%#04X ", Chip8.mem[i]);
     }
+    printf("\n");
+    for (i=0; i<16; i++) {
+        if (i == 8)  printf("\n");
+        if (i != 15) printf("R%d: %#04X - ", i, Chip8.V[i]);
+        else         printf("R%d: %#04X\n", i, Chip8.V[i]);
+    }
+    printf("Index: %#04X\n", Chip8.I);
+    usleep(50000);
 }
 
 int load_rom (char *filename) {
@@ -94,6 +106,19 @@ int cycle (void) {
     Chip8.op = Chip8.mem[Chip8.pc] << 8 | Chip8.mem[Chip8.pc + 1];
     printf("Opcode received: %#04x\n", Chip8.op);
     switch (Chip8.op & 0xF000) {
+        case 0x0000:
+            switch (Chip8.op & 0x000F) {
+                case 0x000E: //Returns from a subroutine
+                    Chip8.stk_ptr--;
+                    Chip8.pc = Chip8.stack[Chip8.stk_ptr];
+                    Chip8.pc += 2;
+                    printf("Returned from subroutine, now at %#04x\n", Chip8.pc);
+                    break;
+                default:
+                    printf("I don't know opcode %#04x, teach me!\n", Chip8.op);
+                    return 1;
+            }
+            break;
         case 0x2000: //Jump to subroutine at NNN
             Chip8.stack[Chip8.stk_ptr] = Chip8.pc;
             Chip8.stk_ptr++;
@@ -112,17 +137,90 @@ int cycle (void) {
             Chip8.pc += 2;
             printf("Moved ahead to: %#04x\n", Chip8.pc);
             break;
+        case 0x7000: //Adds NN to VX
+            Chip8.V[(Chip8.op & 0x0F00) >> 8] += Chip8.op & 0x00FF;
+            Chip8.pc += 2;
+            printf("Added %#04x to V[%d]\n", Chip8.op & 0x00FF, (Chip8.op & 0x0F00) >> 8);
+            printf("Moved ahead to: %#04x\n", Chip8.pc);
+            break;
+        case 0x3000: //Skips the next instruction if VX == NX
+            if (Chip8.V[(Chip8.op & 0x0F00) >> 8] == Chip8.op & 0x00FF) {
+                Chip8.pc += 4;
+                printf("V[%d] == %#04x, moving ahead 4\n", (Chip8.op & 0x0F00) >> 8, Chip8.op & 0x00FF);
+            } else {
+                Chip8.pc += 2;
+                printf("Moving ahead to %#04x\n", Chip8.pc);
+            }
+            break;
+        case 0x1000: //Jumps to address NNN
+            Chip8.pc = Chip8.op & 0x0FFF;
+            printf("Jumped to %#04x\n", Chip8.op & 0x0FFF);
+            break;
+        case 0xD000:
+        { //Draws sprites. If a pixel is flipped VX = 1
+            unsigned short x = Chip8.op & 0x0F00 >> 8;
+            unsigned short y = Chip8.op & 0x00F0 >> 4;
+            unsigned short h = Chip8.op & 0x000F;
+            unsigned short px;
+            int xl, yl;
+
+            Chip8.V[0xF] = 0;
+
+            for (yl=0; yl<h; yl++) {
+                px = Chip8.mem[Chip8.I + yl];
+                printf("%#04x\n", px);
+                for (xl=0; xl<8; xl++) {
+                    if ((px & (0x80 >> xl)) != 0) {
+                        if (Chip8.gfx[x+xl+((y+yl)*64)] == 1) {
+                            Chip8.V[0xF] = 1;
+                            printf("Collision detection\n");
+                        }
+                        Chip8.gfx[x+xl+((y+yl)*64)] ^= 1;
+                    }
+                }
+            }
+            Chip8.draw = 1;
+            Chip8.pc  += 2;
+            printf("Moved ahead to: %#04x\n", Chip8.pc);
+            break;
+        }
         default:
             printf("I don't know what to do with opcode: %#04x, teach me!\n", Chip8.op);
             return 1;
     }
+
+    if (Chip8.delay_timer > 0) --Chip8.delay_timer;
+
+    if (Chip8.sound_timer > 0) {
+        if (Chip8.sound_timer == 1)
+            printf("BEEP\n");
+        --Chip8.sound_timer;
+    }
+}
+
+void render(void) {
+    for (int y=0; y<32; ++y) {
+        for (int x=0; x<64; ++x) {
+            if   (Chip8.gfx[(y*64) + x] == 0) printf("O");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 int main (int argc, char **argv) {
     initialize();
     if (load_rom(argv[1]) != 1) {
         while (1) {
-            if (cycle() == 1) break;
+            int last_time = 0;
+            int current_time = SDL_GetTicks();
+            if (current_time > last_time + 60) {
+                if (cycle() == 1) break;
+                read_mem();
+                render();
+                current_time = last_time;
+            }
         }
     }
     return 0;
